49f0a966ecbe5587b54c1d526c90b457f89ef01c
rim_yejun_ <jayjun.rim@gmail.com>
2025-12-02 10:41:57 +0900

    즐겨찾기 응답 수정

--- src/main/java/com/example/baekseokmapbackend/favorite/dto/FavoriteResponse.java
+++ src/main/java/com/example/baekseokmapbackend/favorite/dto/FavoriteResponse.java
@@ -1,6 +1,7 @@
 package com.example.baekseokmapbackend.favorite.dto;
 
 import com.example.baekseokmapbackend.favorite.domain.Favorite;
+import com.example.baekseokmapbackend.map.domain.Floor;
 import com.example.baekseokmapbackend.map.domain.Room;
 import lombok.Getter;
 
@@ -12,13 +13,30 @@ public class FavoriteResponse {
     private String roomNumber;
     private String roomName;
 
+    private Integer buildingId;
+    private String buildingName;
+    private Integer floorId;
+    private Integer floorLevel;
+
     // Favorite 엔티티를 FavoriteResponse DTO로 변환하는 생성자
     public FavoriteResponse(Favorite favorite) {
-        Room room = favorite.getRoom(); // 즐겨찾기에서 Room 정보 가져오기
+        Room room = favorite.getRoom();
 
         this.favoriteId = favorite.getId();
         this.roomId = room.getId();
         this.roomNumber = room.getRoomNumber();
         this.roomName = room.getName();
+
+        // Room -> Floor -> Building 순서로 정보 가져오기
+        Floor floor = room.getFloor();
+        if (floor != null) {
+            this.floorId = floor.getId();
+            this.floorLevel = floor.getLevel();
+
+            if (floor.getBuilding() != null) {
+                this.buildingId = floor.getBuilding().getId();
+                this.buildingName = floor.getBuilding().getName();
+            }
+        }
     }
 }
